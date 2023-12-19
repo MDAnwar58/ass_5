c4260c9e8eeabad4c8180e2001e26945dbcbc901
MDAnwar58 <anwar.saeed656@gmail.com>
2023-12-19 11:42:47 +0600

    main

---
+++ example/ass_5_example.dart
@@ -0,0 +1,6 @@
+import 'package:ass_5/ass_5.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/ass_5_test.dart
@@ -0,0 +1,16 @@
+import 'package:ass_5/ass_5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
